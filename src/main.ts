43505bdc957d19948243bcf94589fc8db00536ca
Razi Mutussin <fakhruz3105@gmail.com>
2022-06-09 22:42:17 +0000

    Setup tailwind css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import './index.css'
+
 import router from './router'
 import { useAuthStore } from './stores/auth'
 
